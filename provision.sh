7a1432268b3f0d72c51d3b2a7339dc9e83439225
Daniel Quinlan <dq@chaosengine.net>
2022-06-15 10:05:17 -0700

    add option to provision free space for Longhorn

--- provision.sh
+++ provision.sh
@@ -3,8 +3,9 @@
 #
 set -euo pipefail
 
+longhornUseWholeDisk=false
 longhornDisk=/dev/sda
-longhornPart=${longhornDisk}1
+longhornPart=${longhornDisk}2
 longhornDir=/var/lib/longhorn
 
 # get SSL CA cert for private registry cache
@@ -26,11 +27,23 @@ grep -q ttyS4 /etc/securetty || echo 'ttyS4' >> /etc/securetty
 # setup disk for Longhorn
 if [[ -b "${longhornDisk}" ]] ; then
   if [[ ! -b "${longhornPart}" ]] ; then
-    cat <<-EOF | parted "${longhornDisk}"
-	print
-	mklabel gpt
-	mkpart primary ext4 0% 100%
-	EOF
+    if [[ "${longhornUseWholeDisk}" == "true" ]] ; then
+      cat <<-EOF | parted "${longhornDisk}"
+	  print
+	  mklabel gpt
+	  mkpart primary ext4 0% 100%
+	  EOF
+    else
+      # create 2nd partition from free space
+      partStart="$(parted -m "${longhornDisk}" unit GB print free \
+	| grep ':free;' \
+	| grep -v '0.00GB:free;$' \
+	| cut -f2 -d:)"
+      if [[ -n "${partStart}" ]] ; then
+        parted "${longhornDisk}" \
+	  mkpart primary ext4 "$partStart" 100%
+      fi
+    fi
     partprobe
     # TODO optimal ext4 options?
     mke2fs -vF -t ext4 -L longhorn-data -m0 "${longhornPart}"
