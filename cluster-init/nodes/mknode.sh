10f979fbaabd7e25a959c99a694136ec71abdd09
Daniel Quinlan <dq@chaosengine.net>
2022-06-20 10:50:57 -0700

    refactor mknode.sh
    - refactor to single template
    - shellcheck clean
    - use yamllint on each run
    - enable NFS client

--- cluster-init/nodes/mknode.sh
+++ cluster-init/nodes/mknode.sh
@@ -5,29 +5,62 @@
 set -eEuo pipefail
 
 nodes=(m900-1 m900-2 m900-3)
-provisionFile="provision.sh"
+nodeTemplate="member-node.tmpl"
+provisionShFile="provision.sh"
+provisionShDestPath="/root/provision.sh"
 kubevipFile="../kube-vip/kube-vip-ds.yaml"
-primaryTemplate="primary-node.tmpl"
-memberTemplate="member-node.tmpl"
+kubevipFileDestPath="/var/lib/rancher/k3s/server/manifests/kube-vip-ds.yaml"
+# TODO get VIP addr from $kubevipFile
+kubeVipAddr="192.168.40.10"
+kubeVipHostname="k8s.s.chaosengine.net"
+serverToken="SECRETSQUIRREL"  # TODO update before prod
+serverPassword="k3os"  # TODO update before prod `openssl passwd -1`
 
-function encode {
-  local -r input="${1:?arg1 is input file}"
+function writeFilesHeader {
+  local -r path="${1:?arg1 is path}"
+  local -r permissions="${2:?arg2 is permissions}"
+  local -r input="${3:?arg3 is input file}"
+
+  cat <<EOF
+  - path: ${path}
+    owner: root:root
+    permissions: '${permissions}'
+    encoding: gz+base64
+    content: |
+EOF
   gzip -9c "$input" \
     | python3 -m base64 \
-    | sed -e 's/^/    /'
+    | sed -e 's/^/      /'
 }
 
+
 for node in "${nodes[@]}" ; do
+  echo -n "${node}..."
   nodeHostName="$node"
-  nodeProvisionSh="$(encode "${provisionFile}")"
-  export nodeHostName nodeProvisionSh
+  nodeProvisionSh="$(writeFilesHeader "$provisionShDestPath" "0744" "${provisionShFile}")"
+  primaryNodeClusterInit=
+  server_url=
+  primaryNodeKubeVipCfg=
   if [[ "$node" == "m900-1" ]] ; then
-    primaryNodeKubeVipCfg="$(encode "${kubevipFile}")"
-    export primaryNodeKubeVipCfg
-    template="$primaryTemplate"
+    echo -n "primary..."
+    primaryNodeKubeVipCfg="$(writeFilesHeader "$kubevipFileDestPath" "0644" "${kubevipFile}")"
+    # shellcheck disable=SC2089
+    primaryNodeClusterInit='- "--cluster-init"'
   else
-    template="$memberTemplate"
+    server_url="server_url: https://${kubeVipAddr}:6443"
   fi
-  envsubst < "${template}" > "${node}.yaml"
+  # export to appease shellcheck, and catch missing vars
+  export nodeHostName nodeProvisionSh serverToken serverPassword
+  export kubeVipAddr kubeVipHostname
+  export primaryNodeKubeVipCfg
+  # shellcheck disable=SC2090
+  export primaryNodeClusterInit  # we want the string literal, not tokenized
+  export server_url  # secondary only
+  envsubst \
+    < "${nodeTemplate}" \
+    | sed -e '/^ *$/ d' \
+    > "${node}.yaml"
+  yamllint "${node}.yaml"
+  echo "done."
 done
 
