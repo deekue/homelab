3dc45e33a5b2cb7d5ac2552e5e57710a68cf4cd2
Daniel Quinlan <dq@chaosengine.net>
2022-06-20 13:15:33 -0700

    nodes: add kube-vip-cloud-provider, update passwd

--- cluster-init/nodes/mknode.sh
+++ cluster-init/nodes/mknode.sh
@@ -8,13 +8,19 @@ nodes=(m900-1 m900-2 m900-3)
 nodeTemplate="member-node.tmpl"
 provisionShFile="provision.sh"
 provisionShDestPath="/root/provision.sh"
+k3sManifestDir="/var/lib/rancher/k3s/server/manifests"
 kubevipFile="../kube-vip/kube-vip-ds.yaml"
-kubevipFileDestPath="/var/lib/rancher/k3s/server/manifests/kube-vip-ds.yaml"
-# TODO get VIP addr from $kubevipFile
-kubeVipAddr="192.168.40.10"
+kubevipFileDestPath="${k3sManifestDir}/kube-vip-ds.yaml"
+kubevipCloudControllerDir="../kube-vip-cloud-provider"
+kubevipCCFile="${kubevipCloudControllerDir}/kube-vip-cloud-controller.yaml"
+kubevipCCFileDestPath="${k3sManifestDir}/kube-vip-cloud-controller.yaml"
+kubevipCCConfigMapFile="${kubevipCloudControllerDir}/configmap.yaml"
+kubevipCCConfigMapFileDestPath="${k3sManifestDir}/kube-vip-cloud-controller-configmap.yaml"
+kubeVipAddr="192.168.40.10"  # TODO get VIP addr from $kubevipFile
 kubeVipHostname="k8s.s.chaosengine.net"
 serverToken="SECRETSQUIRREL"  # TODO update before prod
-serverPassword="k3os"  # TODO update before prod `openssl passwd -1`
+# shellcheck disable=SC2016
+serverPassword='$1$ZDYMI7GJ$0F0YZRwBXP4l3CQItcXMg/'  # update with `openssl passwd -1`
 
 function writeFilesHeader {
   local -r path="${1:?arg1 is path}"
@@ -22,6 +28,7 @@ function writeFilesHeader {
   local -r input="${3:?arg3 is input file}"
 
   cat <<EOF
+  # yamllint disable-line rule:line-length
   - path: ${path}
     owner: root:root
     permissions: '${permissions}'
@@ -41,9 +48,16 @@ for node in "${nodes[@]}" ; do
   primaryNodeClusterInit=
   server_url=
   primaryNodeKubeVipCfg=
+  primaryNodeKubeVipCcCfg=
+  primaryNodeKubeVipCcCm=
   if [[ "$node" == "m900-1" ]] ; then
     echo -n "primary..."
-    primaryNodeKubeVipCfg="$(writeFilesHeader "$kubevipFileDestPath" "0644" "${kubevipFile}")"
+    primaryNodeKubeVipCfg="$(writeFilesHeader \
+      "$kubevipFileDestPath" "0644" "${kubevipFile}")"
+    primaryNodeKubeVipCcCfg="$(writeFilesHeader \
+      "$kubevipCCFileDestPath" "0644" "${kubevipCCFile}")"
+    primaryNodeKubeVipCcCm="$(writeFilesHeader \
+      "$kubevipCCConfigMapFileDestPath" "0644" "${kubevipCCConfigMapFile}")"
     # shellcheck disable=SC2089
     primaryNodeClusterInit='- "--cluster-init"'
   else
@@ -52,7 +66,7 @@ for node in "${nodes[@]}" ; do
   # export to appease shellcheck, and catch missing vars
   export nodeHostName nodeProvisionSh serverToken serverPassword
   export kubeVipAddr kubeVipHostname
-  export primaryNodeKubeVipCfg
+  export primaryNodeKubeVipCfg primaryNodeKubeVipCcCfg primaryNodeKubeVipCcCm
   # shellcheck disable=SC2090
   export primaryNodeClusterInit  # we want the string literal, not tokenized
   export server_url  # secondary only
