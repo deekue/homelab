883816eb5f63f15b56f5bc966200c5e6cec4010f
Daniel Quinlan <dq@chaosengine.net>
2022-06-25 17:50:31 -0700

    fix argocd-apps bugs

--- cluster-critical/build.sh
+++ cluster-critical/build.sh
@@ -4,18 +4,18 @@
 set -eEuo pipefail
 
 depsOrder=(
-sealed-secrets
-cert-manager
-longhorn-system
-traefik
-argocd
-argocd-apps
+kube-system/sealed-secrets
+cert-manager/cert-manager
+longhorn-system/longhorn-system
+kube-system/traefik
+argocd/argocd
+argocd/argocd-apps
 )
 
 # TODO maybe replace this with an "app of apps"
 # https://github.com/argoproj/argocd-example-apps/tree/master/apps
 function genArgoCdApps {
-  pushd `pwd`/argocd-apps
+  pushd `pwd`/argocd-apps > /dev/null
 
   # generate kustomization.yaml header, for ordering
   cat <<EOF > kustomization.yaml
@@ -28,27 +28,33 @@ resources:
 EOF
 
   # generate argocd application manifests
-  for app in "${depsOrder[@]}" ; do
+  for svc in "${depsOrder[@]}" ; do
+    namespace="${svc%%/*}"
+    app="${svc##*/}"
+    appFile="${app}.yaml"
     if [[ "$app" == "argocd-apps" ]] ; then
       continue  # avoid recursion
     fi
-    export app 
+    echo "Generating ArgoCD Application for $app in $namespace"
+    export app namespace  # export for envsubst
     envsubst \
       < argocd-app.yaml.tmpl \
-      > "${app}.yaml" 
+      > "${appFile}"
 
     # add to Kustomize resource list
-    echo "  - $app" >> kustomization.yaml
+    echo "  - $appFile" >> kustomization.yaml
   done
-  popd
+  popd > /dev/null
 }
 
 function applyManifestsInOrder {
   for svc in "${depsOrder[@]}" ; do
-    if [[ -r "$svc/kustomization.yaml" ]] ; then
-      kustomise build "$svc" | kubectl apply -f -
+    app="${svc##*/}"
+    if [[ -r "$app/kustomization.yaml" ]] ; then
+      kustomise build "$app" --reorder=none \
+	| kubectl apply -f -
     else
-      kubectl apply -f "$svc"
+      kubectl apply -f "$app"
     fi
   done
 }
