69e0062b46308a41a47fef5c972b22413979e2aa
Daniel Quinlan <dq@chaosengine.net>
2022-07-16 16:12:59 -0700

    helper script for Longhorn Instance Manager storage network addresses

---
+++ cluster-critical/longhorn-system/getStorageIpAddresses.sh
@@ -0,0 +1,6 @@
+
+kubectl -n longhorn-system get pods \
+    -l longhorn.io/component=instance-manager \
+    -o json \
+  | jq -r  '.items[].metadata.annotations."k8s.v1.cni.cncf.io/network-status"' \
+  | jq -rs '.[] | .[] | select(.name == "kube-system/storage-macvlan-sbr") | .ips[0]'
