c333970d9ce6f7cdc9a308a7964afc665ff8b619
Daniel Quinlan <dq@chaosengine.net>
2022-07-15 22:43:58 -0700

    add date/time prefix to output

--- cluster-init/nodes/provision.sh
+++ cluster-init/nodes/provision.sh
@@ -10,22 +10,25 @@ caCerts="/etc/ssl/certs/ca-certificates.crt"
 registryProxy="http://192.168.40.52:3128"
 k3sRegistries="/etc/rancher/k3s/registries.yaml"
 
-# setup VLAN interfaces
+function log {
+  local -r msg="$*"
 
+  echo "$(date +%Y%m%d-%H%M%S) $msg"
+}
 
 # get the current cert, check if it's already installed
 regProxyCert="$(curl -fs "$registryProxy/ca.crt")"
 if [[ -n "$regProxyCert" ]] ; then
   if ! grep -qwf <(head -2 <<< "$regProxyCert" | tail -1) "$caCerts" ; then
-    echo "Adding Registry Proxy CA cert"
+    log "Adding Registry Proxy CA cert"
     echo "$regProxyCert" >> "$caCerts"
   fi
 else
-  echo "failed to retrieve Registry Proxy CA cert" >&2
+  log "failed to retrieve Registry Proxy CA cert" >&2
 fi
 
 if [[ ! -r "$k3sRegistries" ]] ; then
-  echo "Configure Registry proxy"
+  log "Configure Registry proxy"
   cat <<-EOF > "$k3sRegistries"
 	mirrors:
 	  docker.io:
@@ -46,15 +49,15 @@ if [[ ! -r "$k3sRegistries" ]] ; then
 	EOF
 fi
 
-echo "setup AMT serial console"
+log "setup AMT serial console"
 grep -q ttyS4 /etc/inittab || echo 'ttyS4::respawn:/sbin/getty -L 115200 ttyS4 vt100' >> /etc/inittab
 grep -q ttyS4 /etc/securetty || echo 'ttyS4' >> /etc/securetty
 
-echo "setup disk for Longhorn"
+log "setup disk for Longhorn"
 if [[ -b "${longhornDisk}" ]] ; then
   if [[ "${longhornUseWholeDisk}" == "true" ]] ; then
     if [[ ! -b "${longhornDisk}1" ]] ; then
-      echo "  using whole disk ${longhornDisk} for Longhorn data"
+      log "  using whole disk ${longhornDisk} for Longhorn data"
       cat <<-EOF | parted "${longhornDisk}"
 	print
 	mklabel gpt
@@ -69,11 +72,11 @@ if [[ -b "${longhornDisk}" ]] ; then
       | cut -f2 -d: \
       || true)"
     if [[ -n "${partStart}" ]] ; then
-      echo -n "not using whole disk, finding free space..."
+      log "not using whole disk, finding free space..."
       parted "${longhornDisk}" \
 	mkpart primary ext4 "$partStart" 100%
       partprobe
-      echo "done."
+      log "done."
     fi
   fi
 
@@ -84,7 +87,7 @@ if [[ -b "${longhornDisk}" ]] ; then
     || true)"
   if [[ -n "${longhornPartNum}" ]] ; then
     longhornPart="${longhornDisk}${longhornPartNum}"  # TODO support other parttion naming. eg nvme0n1p1
-    echo "formating ${longhornPart} with ext4"
+    log "formating ${longhornPart} with ext4"
     # TODO optimal ext4 options?
     mke2fs -vF -t ext4 -L longhorn-data -m0 "${longhornDisk}${longhornPartNum}"
   else
@@ -96,24 +99,24 @@ if [[ -b "${longhornDisk}" ]] ; then
     if [[ -n "${longhornPartNum}" ]] ; then
       longhornPart="${longhornDisk}${longhornPartNum}"  # TODO support other parttion naming. eg nvme0n1p1
     else
-      echo "ERROR: partition not found/created on ${longhornDisk} for Longhorn data" >&2
+      log "ERROR: partition not found/created on ${longhornDisk} for Longhorn data" >&2
       exit 1
     fi
   fi
   if [[ ! -d "${longhornDir}" ]] ; then
-    echo "making dir ${longhornDir}"
+    log "making dir ${longhornDir}"
     mkdir -p "${longhornDir}"
   fi
   if [[ -b "${longhornPart}" ]] ; then
     if ! grep -q "${longhornPart}" /etc/fstab ; then
-      echo "updating /etc/fstab"
+      log "updating /etc/fstab"
       grep -v "${longhornDir}" /etc/fstab \
 	> /etc/fstab.tmp
       echo "${longhornPart}	${longhornDir}	ext4	nodiratime,relatime 0 0" \
 	>> /etc/fstab.tmp
       mv /etc/fstab.tmp /etc/fstab
     fi
-    echo "mounting ${longhornPart} on ${longhornDir}"
+    log "mounting ${longhornPart} on ${longhornDir}"
     mount -a
     mount | grep "${longhornPart}"
   fi
