e372efe94fc0584f390bf0bb40ad298875308f5b
Daniel Quinlan <dq@chaosengine.net>
2022-07-17 13:14:59 -0700

    remove docker-cni-plugins, extra nads

--- cluster-critical/longhorn-system/getStorageIpAddresses.sh
+++ cluster-critical/longhorn-system/getStorageIpAddresses.sh
@@ -3,4 +3,4 @@ kubectl -n longhorn-system get pods \
     -l longhorn.io/component=instance-manager \
     -o json \
   | jq -r  '.items[].metadata.annotations."k8s.v1.cni.cncf.io/network-status"' \
-  | jq -rs '.[] | .[] | select(.name == "kube-system/storage-macvlan-sbr") | .ips[0]'
+  | jq -rs '.[] | .[] | select(.name == "kube-system/storage-macvlan-whereabouts") | .ips[0]'
