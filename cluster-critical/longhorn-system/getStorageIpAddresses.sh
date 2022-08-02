0b2de626e86e38123dc7747f0d3420af6357fcc4
Daniel Quinlan <dq@chaosengine.net>
2022-08-02 13:56:29 -0700

    remove Longhorn

--- cluster-critical/longhorn-system/getStorageIpAddresses.sh
+++
@@ -1,6 +0,0 @@
-
-kubectl -n longhorn-system get pods \
-    -l longhorn.io/component=instance-manager \
-    -o json \
-  | jq -r  '.items[].metadata.annotations."k8s.v1.cni.cncf.io/network-status"' \
-  | jq -rs '.[] | .[] | select(.name == "kube-system/storage-macvlan-whereabouts") | .ips[0]'
